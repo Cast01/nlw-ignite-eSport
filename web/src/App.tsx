b40efaed76c425c4dfd2f3db2e195c269f8719fa
Cast01 <gabriel_floriano_castilho@outlook.com>
2022-09-28 22:00:24 -0300

    Configurando o Slider

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,6 +2,11 @@ import { useEffect, useState } from 'react';
 
 import * as Dialog from '@radix-ui/react-dialog';
 
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, A11y } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+
 import { GameBanner } from './components/GameBanner';
 import { CreateAdBanner } from './components/CreateAdBanner';
 import { FormModal } from './components/FormModal';
@@ -30,7 +35,7 @@ function App() {
   }, []);
 
   return (
-    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20">
+    <div className="max-w-[1344px] mx-auto flex flex-col items-center my-20 px-3">
       <img src={LogoImg} alt="" />
 
       <h1 className="text-6xl text-white font-black mt-20">Seu&nbsp;
@@ -40,15 +45,24 @@ function App() {
         &nbsp;está aqui.
       </h1>
 
-      <div className="grid grid-cols-6 gap-6 mt-16">
+      <Swiper
+        className="w-full mt-16"
+        modules={[Navigation, A11y]}
+        slidesPerView={6}
+        navigation
+        onSwiper={(swiper: any) => console.log(swiper)}
+        onSlideChange={() => console.log('slide change')}
+      >
         {
           games.map(game => {
             return (
-              <GameBanner key={game.id} bannerUrl={game.bannerUrl} title={game.title} adsCount={game._count.Ads} />
+              <SwiperSlide className="flex justify-center">
+                <GameBanner key={game.id} bannerUrl={game.bannerUrl} title={game.title} adsCount={game._count.Ads} />
+              </SwiperSlide>
             );
           })
         }
-      </div>
+      </Swiper>
 
       <Dialog.Root>
         <CreateAdBanner />
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -14,6 +14,9 @@ module.exports = {
         "duo-gradient": "linear-gradient(89.86deg, #9572FC 16.08%, #43E7AD 50.94%, #E1D55D 72.57%)",
         "game-gradient": "linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%)",
       },
+      width: {
+        slideAdjuste: "calc(100% - 54px)"
+      }
     },
   },
   plugins: [],
